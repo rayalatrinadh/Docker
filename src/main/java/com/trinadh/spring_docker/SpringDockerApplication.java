704d94128c580c64a3822dcdead32a4a9ac76413
Trinadh Rayala <3nadhmail@gmail.com>
2024-08-28 14:36:50 -0500

    greetings end point

--- src/main/java/com/trinadh/spring_docker/SpringDockerApplication.java
+++ src/main/java/com/trinadh/spring_docker/SpringDockerApplication.java
@@ -2,12 +2,14 @@ package com.trinadh.spring_docker;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
 public class SpringDockerApplication {
 
+	@GetMapping("/greeting")
 	public String greeting(){
 		return "Welcome to Docker World.";
 	}
